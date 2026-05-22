c14fe34d6ad16a16173129eb23bc5973c90b6ff7
Stefan Dörig <sdoerig@bluewin.ch>
2026-05-22 21:34:23 +0200

    removed nested loops
    Signed-off-by: Stefan Dörig <sdoerig@bluewin.ch>

--- carpathia-core/src/generator/template_engine.rs
+++ carpathia-core/src/generator/template_engine.rs
@@ -36,32 +36,39 @@ impl TemplateEngine {
         tera.render(template_name, &ctx)
     }
 }
+
 /// Returning all the types found in the database schema - the users need this to
 /// create their own mapping.
+/// 
+/// If there is a mapping file provided, the old mapping is merged into the
+/// new mapping structure.
 pub fn get_db_types(
     config: &CarpathiaConfig,
     table_info_map: &AbstractDbRepr,
 ) -> Result<Types, CarpathiaError> {
-    let mut types = Types::new();
     if table_info_map.tables.is_empty() {
         return Err(CarpathiaError {
             message: "No tables found".to_string(),
             error_type: ErrorNumber::NoDbObjectsDiscovered,
         });
     }
-    for key in table_info_map.tables.keys() {
-        for attribute in table_info_map.tables[key].attributes.values() {
+
+    let mut types = Types::new();
+
+    // Chaining loops - otherwhise I had to create loop in loop.
+    table_info_map.tables.values()
+        .flat_map(|table| table.attributes.values())
+        .for_each(|attribute| {
             let u_import_old = config
                 .type_map
                 .type_mapping
                 .get(&attribute.data_type)
-                .or(Some(DEFAULT_TYPE_MAPPING))
                 .unwrap_or(DEFAULT_TYPE_MAPPING);
-            types
-                .type_mapping
+
+            types.type_mapping
                 .entry(attribute.data_type.clone())
-                .or_insert(u_import_old.clone());
-        }
-    }
+                .or_insert_with(|| u_import_old.clone());
+        });
+
     Ok(types)
 }
